e3a217fc3d3526a901535996e433e93f5189de15
brendyu <bmuye13@gmail.com>
2024-07-05 23:09:06 -1000

    Used linting

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 console.log("Hi! Welcome to my UI/UX Portfolio!")
 function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-      x.className += ' responsive';
-    } else {
-      x.className = 'navtoggle';
-    }
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
   }
+}
